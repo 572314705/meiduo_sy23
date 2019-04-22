74577779e3bc8ae7af1f9cb7307698f9b19ad430
ldy <572314705@qq.com>
2019-04-22 21:38:22 +0800

    配置静态文件

--- meiduo_mall/meiduo_mall/settings/dev.py
+++ meiduo_mall/meiduo_mall/settings/dev.py
@@ -156,7 +156,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-
+# 配置静态文件加载路径
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 #缓存
 CACHES = {
