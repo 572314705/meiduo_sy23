7320926df9152860d48607d453556080b31be68c
ldy <572314705@qq.com>
2019-04-30 11:49:27 +0800

    收货地址－地区缓存

--- meiduo_mall/meiduo_mall/apps/areas/views.py
+++ meiduo_mall/meiduo_mall/apps/areas/views.py
@@ -3,6 +3,8 @@ from django.views import View
 from meiduo_mall.utils.response_code import RETCODE
 from django import http
 from .models import Area
+from django.core.cache import cache
+from . import contants
 
 
 class AreasView(View):
@@ -14,50 +16,59 @@ class AreasView(View):
         # 验证 : area_id　允许为空，表示查询省信息
         # 处理
         if area_id is None:
-            # 查询省信息
-            provinces = Area.objects.filter(parent__isnull=True)
-            # 转换成字典
-            province_list = []
-            for province in provinces:
-                province_list.append({
-                    'id' : province.id,
-                    'name':province.name,
-                })
-
+            # 读取缓存
+            province_list = cache.get('province_list')
+            if province_list is None:
+                # 查询省信息
+                provinces = Area.objects.filter(parent__isnull=True)
+                # 转换成字典
+                province_list = []
+                for province in provinces:
+                    province_list.append({
+                        'id': province.id,
+                        'name': province.name,
+                    })
+                # 写缓存
+                cache.set('province_list', province_list, contants.AREA_CACHE_EXPIRES)
+            # 响应
             return http.JsonResponse({
-                'code':RETCODE.OK,
-                'errmsg':'ok',
-                'province_list':province_list,
+                'code': RETCODE.OK,
+                'errmsg': 'ok',
+                'province_list': province_list,
             })
         else:
-            # 查询指定编码地区及子及地区
-            try:
-                area = Area.objects.get(pk = area_id)
-            except:
-                return http.JsonResponse({
-                    'code':RETCODE.PARAMERR,
-                    'errmsg':'地区编码无效',
+            sub_data = cache.get('area_' + area_id)
+            if sub_data is None:
+                # 查询指定编码地区及子及地区
+                try:
+                    area = Area.objects.get(pk=area_id)
+                except:
+                    return http.JsonResponse({
+                        'code': RETCODE.PARAMERR,
+                        'errmsg': '地区编码无效',
 
-                })
-            else:
-                # 获取子级地区
-                subs = area.subs.all()
-                sub_list = []
-                for sub in subs:
-                    sub_list.append({
-                        'id':sub.id,
-                        'name':sub.name
                     })
-                # 结果数据
-                sub_data = {
-                    'id':area.id,
-                    'name':area.name,
-                    'subs':sub_list
+                else:
+                    # 获取子级地区
+                    subs = area.subs.all()
+                    sub_list = []
+                    for sub in subs:
+                        sub_list.append({
+                            'id': sub.id,
+                            'name': sub.name
+                        })
+                    # 结果数据
+                    sub_data = {
+                        'id': area.id,
+                        'name': area.name,
+                        'subs': sub_list
 
-                }
+                    }
+                    # 写缓存
+                    cache.set('area_' + area_id, sub_data, contants.AREA_CACHE_EXPIRES)
                 return http.JsonResponse({
-                    'code':RETCODE.OK,
-                    'errmsg':'ok',
-                    'sub_data':sub_data
+                    'code': RETCODE.OK,
+                    'errmsg': 'ok',
+                    'sub_data': sub_data
                 })
                 # 响应
