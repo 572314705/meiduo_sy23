2fbbd69844a13455286b13d1e8cd885f6482dc3f
ldy <572314705@qq.com>
2019-04-30 10:29:08 +0800

    收货地址－创建一个新表－倒入数据mysql -h127.0.0.1 -uroot -pmysql meiduo_sy23 < areas.sql

--- meiduo_mall/meiduo_mall/apps/areas/models.py
+++ meiduo_mall/meiduo_mall/apps/areas/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Area(models.Model):
+
+    name = models.CharField(max_length=30)
+    parent = models.ForeignKey('self',null=True,related_name='subs')
+
+    class Meta:
+        db_table = 'tb_areas'
